29c54cd75d5e24db154cf5be5321403039026d87
aleusa <aleusa2013@gmail.com>
2021-08-28 16:08:48 -0400

    notifications
    notifications

--- App.js
+++ App.js
@@ -7,6 +7,8 @@
  */
 
 import * as React from 'react';
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
 import { useLayoutEffect, useEffect, useState } from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -157,7 +159,86 @@ function Messages() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Messages!</Text>
+      <Button
+      title="Notification"
+      onPress={() => { handleNotification() }     
+      }
+    />
     </View>
+  );
+}
+
+// Must be outside of any component LifeCycle (such as `componentDidMount`).
+// PushNotification.configure({
+//   // (optional) Called when Token is generated (iOS and Android)
+//   onRegister: function (token) {
+//     console.log("TOKEN:", token);
+//   },
+
+//   // (required) Called when a remote is received or opened, or local notification is opened
+//   onNotification: function (notification) {
+//     console.log("NOTIFICATION:", notification);
+
+//     // process the notification
+
+//     // (required) Called when a remote is received or opened, or local notification is opened
+//     notification.finish(PushNotificationIOS.FetchResult.NoData);
+//   },
+
+//   // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+//   onAction: function (notification) {
+//     console.log("ACTION:", notification.action);
+//     console.log("NOTIFICATION:", notification);
+
+//     // process the action
+//   },
+
+//   // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+//   onRegistrationError: function(err) {
+//     console.error(err.message, err);
+//   },
+
+//   // IOS ONLY (optional): default: all - Permissions to register.
+//   permissions: {
+//     alert: true,
+//     badge: true,
+//     sound: true,
+//   },
+
+//   // Should the initial notification be popped automatically
+//   // default: true
+//   popInitialNotification: true,
+
+//   /**
+//    * (optional) default: true
+//    * - Specified if permissions (ios) and token (android and ios) will requested or not,
+//    * - if not, you must call PushNotificationsHandler.requestPermissions() later
+//    * - if you are not using remote notification or do not have Firebase installed, use this:
+//    *     requestPermissions: Platform.OS === 'ios'
+//    */
+//   requestPermissions: Platform.OS === 'ios'
+//   //requestPermissions: true,
+// });
+
+const handleNotification = () => {
+  PushNotification.localNotification ({
+    channelId: "channel-id",
+    title: "Test",
+    message: "Test message"
+  });
+}
+
+const createChannels = () => {
+  PushNotification.createChannel(
+    {
+      channelId: "channel-id", // (required)
+      channelName: "My channel", // (required)
+      channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
+      playSound: false, // (optional) default: true
+      soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
+      vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+    },
+    (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
   );
 }
 
